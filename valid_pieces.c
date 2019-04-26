a90221a2fab0e09f358b26987c849de2462254f6
Mohammad Butt <mbutt@student.42.us.org>
2019-04-25 21:15:09 -0700

    Stored valid pieces

--- valid_pieces.c
+++ valid_pieces.c
@@ -6,37 +6,113 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 20:07:29 by mbutt             #+#    #+#             */
-/*   Updated: 2019/04/25 20:28:48 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/04/25 21:07:22 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+/*
+int main (void)
+{
+	char *p01;
+	char *p02;
+	char *p03;
+	char *p04;
+	char *p05;
+	char *p06;
+	char *p07;
+
+	p01 = (char *)malloc(20);
+	p02 = (char *)malloc(20);
+	p03 = (char *)malloc(20);
+	p04 = (char *)malloc(20);
+	p05 = (char *)malloc(20);
+	p06 = (char *)malloc(20);
+	p07 = (char *)malloc(20);
+
+	p01 = "###.\n.#..\n....\n....\n";
+	p02 = "#...\n#...\n#...\n#...\n";
+	p03 = "##..\n#...\n#...\n....\n";
+	p04 = "#...\n##..\n#...\n....\n";
+	p05 = "#...\n#...\n##..\n....\n";
+	p06 = ".#..\n##..\n#...\n....\n";
+	p07 = "##..\n##..\n....\n....\n";
+	
+//	printf("%s\n", p01);
+//	printf("%s\n", p02);
+//	printf("%s\n", p03);
+//	printf("%s\n", p04);
+//	printf("%s\n", p05);
+//	printf("%s\n", p06);
+//	printf("%s\n", p07);
+
+	return(0);
+}
+*/
+
+/*
+int main (void)
+{
+	char *p08;
+	char *p09;
+	char *p10;
+	char *p11;
+	char *p12;
+	char *p13;
+
+	p08 = (char *)malloc(20);
+	p09 = (char *)malloc(20);
+	p10 = (char *)malloc(20);
+	p11 = (char *)malloc(20);
+	p12 = (char *)malloc(20);
+	p13 = (char *)malloc(20);
+
+	p08 = "###.\n#...\n....\n....\n";
+	p09 = "#...\n##..\n.#..\n....\n";
+	p10 = "#...\n###.\n....\n....\n";
+	p11 = ".#..\n.#..\n##..\n....\n";
+	p12 = ".#..\n##..\n.#..\n....\n";
+	p13 = ".##.\n##..\n....\n....\n";
+
+//	printf("%s\n", p08);
+//	printf("%s\n", p09);
+//	printf("%s\n", p10);
+//	printf("%s\n", p11);
+//	printf("%s\n", p12);
+//	printf("%s\n", p13);
+
+	return 0;
+}
+*/
 
 int main (void)
 {
-	char *piece01;
-	char *piece02;
-	char *piece03;
-	char *piece04;
-	char *piece05;
+	char *p14;
+	char *p15;
+	char *p16;
+	char *p17;
+	char *p18;
+	char *p19;
 
-	piece01 = (char *) malloc(20);
-	piece02 = (char *) malloc(20);
-	piece03 = (char *) malloc(20);
-	piece04 = (char *) malloc(20);
-	piece05 = (char *) malloc(20);
+	p14 = (char *)malloc(20);
+	p15 = (char *)malloc(20);
+	p16 = (char *)malloc(20);
+	p17 = (char *)malloc(20);
+	p18 = (char *)malloc(20);
+	p19 = (char *)malloc(20);
 
-	piece01 = "###.\n.#..\n....\n....\n";
-	piece02 = "#...\n#...\n#...\n#...\n";
-	piece03 = "##..\n#...\n#...\n....\n";
-	piece04 = "#...\n##..\n#...\n....\n";
-	piece05 = "#...\n#...\n##..\n....\n";
-/*	
-	printf("%s\n", piece01);
-	printf("%s\n", piece02);
-	printf("%s\n", piece03);
-	printf("%s\n", piece04);
-	printf("%s\n", piece05);
-*/
-	return(0);
+	p14 = ".#..\n###.\n....\n....\n";
+	p15 = "##..\n.#..\n.#..\n....\n";
+	p16 = "##..\n.##.\n....\n....\n";
+	p17 = "..#.\n###.\n....\n....\n";
+	p18 = "###.\n..#.\n....\n....\n";
+	p19 = "####\n....\n....\n....\n";
+
+	printf("%s", p14);
+	printf("%s", p15);
+	printf("%s", p16);
+	printf("%s", p17);
+	printf("%s", p18);
+	printf("%s", p19);
+
 }

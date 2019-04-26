aedcca4f01d69ba5da411ba8f7be6b79691bec15
Mohammad Butt <mbutt@student.42.us.org>
2019-04-25 19:33:31 -0700

    Can open, read, and print pieces. Need to validate pieces.

--- fillit.c
+++ fillit.c
@@ -6,10 +6,47 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 15:39:58 by mbutt             #+#    #+#             */
-/*   Updated: 2019/04/25 15:43:08 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/04/25 19:32:21 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "fillit.h"
 
+int main (void)
+{
+/*
+	FILE *fd1;
+	char *line = NULL;
+	size_t buff_size = 0;
+	int ret1;
 
+
+	fd1 = fopen("tests/corrects/valid_00", "r");
+	while((ret1 = getline(&line, &buff_size, fd1)))
+	{
+		if (ret1 > 0)
+		{
+			printf("%s", line);
+		}
+		else
+			break;
+	}
+*/
+	int fd1;
+//	int ret1;
+	int bytes_read;
+	int buff_size = 1;
+//	char *one_line;
+	char placeholder[buff_size];
+	//char placeholder[buff_size + 1];
+	fd1 = open("tests/corrects/valid_06", O_RDONLY);
+	//bytes_read = read(fd1, placeholder, buff_size);
+	while((bytes_read = read(fd1, placeholder, buff_size) > 0))
+	{
+		placeholder[buff_size] = '\0';
+		printf("%s", placeholder);
+	}
+
+	return(0);
+
+}

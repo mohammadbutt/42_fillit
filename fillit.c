6bfdffa033bd84d0d1d19c46c3408564fc44deeb
Mohammad Butt <mbutt@student.42.us.org>
2019-04-26 15:50:11 -0700

    Figuring out how to compare pieces

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 15:39:58 by mbutt             #+#    #+#             */
-/*   Updated: 2019/04/26 15:08:15 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/04/26 15:49:37 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,43 @@ int main (void)
 	int fd1;
 //	int ret1;
 	int bytes_read;
-	int buff_size = 1;
+//	int buff_size = 1;
+	int buff_size = 20;
 //	char *one_line;
 	char placeholder[buff_size];
+	char user_piece[20];
+	int i = 0;
 	//char placeholder[buff_size + 1];
-	fd1 = open("tests/corrects/valid_20", O_RDONLY);
-	//bytes_read = read(fd1, placeholder, buff_size);
+//	fd1 = open("tests/corrects/valid_20", O_RDONLY);
+	fd1 = open("valid_pieces/valid_01", O_RDONLY);	
+//bytes_read = read(fd1, placeholder, buff_size);
+/*
 	while((bytes_read = read(fd1, placeholder, buff_size) > 0))
 	{
 		placeholder[buff_size] = '\0';
 		printf("%s", placeholder);
+	}
+*/
+	while((bytes_read = read(fd1, placeholder, buff_size) > 0))
+	{
+		placeholder[buff_size] = '\0';
+		ft_strcpy(user_piece, placeholder);
+//		user_piece[i] = *placeholder;
+//
+		
+		i++;
+//		printf("")
+	}
+	printf("%s\n\n", placeholder);
+	
+	if(ft_strcmp(p01, user_piece) == 0)
+	{
+		printf("%s", user_piece);
+		printf("\nuser piece matches");
+	}
+	else
+	{
+		printf("\nError, piece does not match");
 	}
 
 	return(0);

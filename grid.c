081451ffb555bf1be6b3d1c7080d4b3b0feae51c
Mohammad Butt <mbutt@student.42.us.org>
2019-04-27 16:14:57 -0700

    Grid function almost works

--- grid.c
+++ grid.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 14:46:13 by mbutt             #+#    #+#             */
-/*   Updated: 2019/04/27 15:18:57 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/04/27 16:14:15 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 
 char **ft_grid(int width)
 {
-	int default_width;
+	int new_width;
 	int height;
 	int i;
 	int j;
 	char **empty_grid;
-//	int default_width;
-//	int height;
 
-	default_width = width;
+	new_width = width;
 	i = 0;
 	j = 0;
+	height = width;
 	
-	empty_grid = (char **)malloc(sizeof(char *) * (default_width + 1));
+	empty_grid = (char **)malloc(sizeof(char *) * (width));
 
 	while(width)
 	{
-		empty_grid[i] = (char *)malloc(sizeof(char) *(default_width + 1));
+		empty_grid[i] = (char *)malloc(sizeof(char) * (width + 1));
 		i++;
 		width--;
 	}
@@ -39,16 +38,16 @@ char **ft_grid(int width)
 
 	while(height)
 	{
-		while(width)
+		while(new_width)
 		{
 			empty_grid[i][j] = '.';
 			j++;
-			width--;
+			new_width--;
 		}
 		empty_grid[i][j] = '\n';
 		j = 0;
 		i++;
-		width = default_width;
+		new_width = width;
 		height--;
 	}
 	return(empty_grid);
@@ -57,12 +56,22 @@ char **ft_grid(int width)
 int main (void)
 {
 	char **empty_grid;
-	empty_grid = ft_grid(4);
-	//ft_grid(4);
-	//printf("%s", *ft_grid(4));
-	printf("%s", empty_grid[0]);
-	printf("%s", empty_grid[1]);
-	printf("%s", empty_grid[2]);
-	printf("%s", empty_grid[3]);
+	int num;
+	int i;
 
+	num = 4;
+	i = 0;
+	empty_grid = ft_grid(num);
+	
+//	ft_putstr(empty_grid[0]);
+//	ft_putstr(empty_grid[1]);
+//	ft_putstr(empty_grid[2]);
+//	ft_putstr(empty_grid[3]);
+
+	while(i < num)
+	{
+//		printf("%s", empty_grid[i]);
+		ft_putstr(empty_grid[i]);
+		i++;
+	}
 }

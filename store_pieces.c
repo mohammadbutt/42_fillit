0539cb5afc858d71dfdbb4c629b867a58a7d3be1
Mohammad Butt <mbutt@student.42.us.org>
2019-05-01 16:16:29 -0700

    storing files

--- store_pieces.c
+++ store_pieces.c
@@ -6,12 +6,12 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 15:47:41 by mbutt             #+#    #+#             */
-/*   Updated: 2019/05/01 15:53:17 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/05/01 16:16:04 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
+#include "fillit.h"
+#include <stdio.h> // for printf
 /*
 ** We can assume that pieces are valid.
 ** pieces that are valid are stored in a 2D array as numbers. Not linked list.
@@ -21,5 +21,21 @@
 
 int main (void)
 {
+	int fd;
+	int ret1;
+	char stored_grid[21];
+	char placeholder[20 + 1];
+	char *one_line;
+	fd = open("valid1.txt", O_RDONLY);
 
+	while((ret1 = read(fd, placeholder, 20)) == 20)
+	{
+		ft_strcpy(stored_grid, placeholder);
+//		break;
+	}
+
+	printf("%s", stored_grid);
+	printf("%s", placeholder);
+
+	return(0);
 }

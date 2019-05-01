d8707b6dff967054d48cd1420250b158a3586e2c
Mohammad Butt <mbutt@student.42.us.org>
2019-05-01 15:54:09 -0700

    Created a file for valid pieces

---
+++ store_pieces.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   store_pieces.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/01 15:47:41 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/01 15:53:17 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** We can assume that pieces are valid.
+** pieces that are valid are stored in a 2D array as numbers. Not linked list.
+** We will use malloc twice. Malloc once to malloc number of pieces. Malloc
+** the second time to malloc each character in a 2d array.
+*/
+
+int main (void)
+{
+
+}
